833d31ef923b734414a10316a50bfcfc51216f88
Hasan Alperen SELÇUK <78741582+alperen-selcuk@users.noreply.github.com>
2024-07-01 13:39:30 +0300

    Create app.js

---
+++ app/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
