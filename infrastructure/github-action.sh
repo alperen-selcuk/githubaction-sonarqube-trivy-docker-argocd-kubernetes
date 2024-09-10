003d20075506685f851ef908640621e8cd91f48f
Hasan Alperen SELÇUK <78741582+alperen-selcuk@users.noreply.github.com>
2024-09-10 11:01:39 +0300

    Update github-action.sh

--- infrastructure/github-action.sh
+++ infrastructure/github-action.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+mkdir actions-runner && cd actions-runner
+
+#version can be different.
+
+curl -o actions-runner-linux-x64-2.308.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.308.0/actions-runner-linux-x64-2.308.0.tar.gz
+tar xzf ./actions-runner-linux-x64-2.308.0.tar.gz
+./config.sh --url https://github.com/your-username/your-repo --token YOUR_REGISTRATION_TOKEN
+sudo ./svc.sh install
+sudo ./svc.sh start
