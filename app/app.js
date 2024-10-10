8f9645c2fce1189a26a14423c31e4a4b7e7e3758
Hasan Alperen SELÇUK <78741582+alperen-selcuk@users.noreply.github.com>
2024-10-10 20:53:18 +0300

    Update app.js

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello Bayraktar!');
 });
 
 app.get('/health', (req, res) => {
